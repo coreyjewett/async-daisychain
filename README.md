da3083556e98a4e3c82121bc9b854a96d940af85
Corey Jewett <cj@syntheticplayground.com>
2012-11-16 04:57:33 -0800

    bug fixes, docs, ready for point release.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 async-daisychain
 ================
 
-Easily create and manage daisychains of async queues.
+Easily create and manage a chain of async queues. Each queue worker may emit an error or a single memo object to it's callback. just like async#push, an error is handed to the callback given. In the case of a memo, it is forwarded to the next queue. When the last queue finishes the resulting memo is passed to the originating callback.
+
+    var daisychain = require("async-daisychain");
+    // see test/test.js for a couple examples
+
+retry
+-----
+
+When an error is passed to the callback the second argument is the memo from the last successful worker and the third argument passed to the callback is the queue_id for the failed queue. These arguments, plus a callback, may be passed to #retry to reinvoke the failed queue and remaining queues on the memo. In a sense, this is reminiscent of ruby's *retry* statement and was inspired by it. This is particularly handy where a worker might fail due to intermittent IO issues. In most cases it also would make sense to delay for some interval before retrying, but this is not built in currently.
+
+Argument Against
+----------------
+Functionally, this is similar to an async *queue* whose worker spawns a *waterfall* for each task. That strategy is actually more flexible as it is not limited to a single memo. On the upside, the added complexity herein provides the retry mechanics and also has opportunity to be improved by having mixed concurrency and also an intellgent backpressure system that has visibility into the whole system, not just it's head.
--- test/test.js
+++ test/test.js
@@ -1,31 +1,58 @@
 var daisychain = require('../lib/index');
 var assert = require('assert');
 
+//// A Harness of sorts ////
+var tests_remaining = 19;
+function completed() {
+  tests_remaining--;
+}
+
+process.on("exit", function() {
+  if (tests_remaining !== 0) {
+    assert.fail(tests_remaining, 0, "Exited, but " + tests_remaining + " test(s) did not call complete().");
+  } else if (tests_remaining < 0) {
+    assert.fail(0, tests_remaining, Math.abs(tests_remaining) + " tests too many ran.");
+  } else {
+    // All OK
+  }
+});
+
+
+//// Workers ////
 function sq(n, callback) {
-  if (n > Math.pow(3, 16)+1) return callback("Too Big");
   callback(null, n * n);
 }
 
-var tests = 9, incomplete_mon = setTimeout(function(){ assert.fail(0, tests, "Not all assertions exercised"); }, 1000);
-function completed(is_last) {
-  tests--;
-  if (tests == 0) {
-    clearTimeout(incomplete_mon);
-  }
-
-  if (tests < 0) {
-    assert.fail(0, tests, "Test count is wrong.");
-  }
-
-  if (is_last && tests !== 0) {
-    assert.fail("complete(true) called prematurely", tests);
-  }
+function plus1(n, callback) {
+  if (n == 5) return callback("Got 5");
+  callback(null, n + 1);
 }
+
+
+//// Tests ////
+var dc3 = daisychain([
+     function(memo, cb) { if (memo != "A") return cb("A Expected"); cb(null, "B"); }
+    ,function(memo, cb) { if (memo != "B") return cb("B Expected"); cb(null, "C"); }
+    ,function(memo, cb) { if (memo != "C") return cb("C Expected"); cb(null, "D"); }
+    ,function(memo, cb) { if (memo != "D") return cb("D Expected"); cb(null, "E"); }
+  ], 1);
+dc3.push("A", function(err, result) {
+  assert.ifError(err);
+  assert.equal(result, "E");
+  completed();
+})
+
+dc3.push("B", function(err, result) {
+  assert.equal(err, "A Expected");
+  completed();
+})
+
+
 
 var dc = daisychain([sq, sq, sq, sq], 1);
 dc.saturated = completed;
 dc.empty = completed;
-dc.drain = completed.bind(null, true);
+dc.drain = completed;
 
 dc.push(2, function(err, result) {
   assert.ifError(err);
@@ -39,15 +66,87 @@ dc.push(3, function(err, result) {
   completed();
 });
 
-dc.push(256, function(err, result) {
-  assert.equal(err, "Too Big");
-  completed();
-});
-
 dc.push([2,2,2], function(err, result) {
   assert.ifError(err);
   assert.equal(result, Math.pow(2, 16));
   completed();
 });
 
-assert.equal(dc.length(), 6);
+assert.equal(dc.length(), 5);
+
+
+
+var dc4 = daisychain(
+  [
+    // this function will only work once, every successive call will fail.
+    function(memo, cb) { 
+      if (!this.toggle) {
+        this.toggle = true;
+        cb(null, "y");
+      } else {
+        cb(new Error("No, no, no!"));
+      }
+    }
+    // this function has a repeating pattern of fail, succeed, fail, etc.
+    ,function(memo, cb) {
+      this.toggle = !this.toggle; 
+      if (this.toggle) {
+        cb(new Error("Uh, oh"));
+      } else {
+        cb(null, "z");
+      }
+    }
+  ], 1);
+dc4.push("x", function dc4_cb(err, result, queue_id) {
+  if (err) {    // first time daisy chain tells us worker #2 failed.
+    assert.equal(result, "y");
+    assert.equal(queue_id, 1);    // first worker succeeded, second failed, retry should be the second.
+    dc4.retry(queue_id, result, dc4_cb);
+    return;
+  }
+  // second time is a success.
+  assert.equal(result, "z");
+  completed();
+})
+
+
+//// Allowing three simulatenous task processors and a total of six processes, simulate a temporary failing io effort.
+var seeit = false;
+var dc5 = daisychain(
+  [
+    // this function just passes the memo through, but we can take a peek if we want. (seeit=true)
+    function(memo, cb) {
+      process.nextTick(function(){
+        setTimeout(function(){
+          seeit && console.log("HELLO:", memo);
+          cb(null, memo);
+        }, 10);
+      });
+    }
+    // this function fails 12 times then succeeds.
+    ,function(memo, cb) {
+      this.count = (this.count || 0) +1; 
+      if (this.count > 12) {
+        seeit && console.log("SUCCESS["+this.count+"]:", memo);
+        cb(null, [memo, this.count]);
+      } else {
+        var delay = this.count + 10; // non-deterministic; 1-10ms random delay use: parseInt(Math.random()*10)+1;
+        seeit && console.log("DOWN["+this.count+"]:", memo);
+        // pretend server is down after a brief delay of trying to connect.
+        process.nextTick(function() {
+          setTimeout(function(){
+            cb(new Error("server down"));
+          }, delay);
+        });
+      }
+    }
+  ], 3);
+dc5.push(["X", "Y", "Z", "P", "D", "Q", "Jane", "John"], function dc5_cb(err, result, queue_id) {
+  if (err) {
+    assert.equal(queue_id, 1);
+    dc5.retry(queue_id, result, dc5_cb);
+    return;
+  }
+  completed();
+})
+
